66630425580cc680237cade312e0c32768cc3f13
BarenAD <baren1998@gmail.com>
2023-01-03 15:15:10 +0700

    development: fix style footer

--- src/components/main/Footer/Footer.tsx
+++ src/components/main/Footer/Footer.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import React, {FC} from "react";
 
 import "./Footer.scss"
 import {useAppSelector} from "../../../redux/hooks";
@@ -6,6 +6,7 @@ import {STORE_STATUSES} from "../../../config/storeStatuses";
 import {SITE_CONFIG_IDENTIFIERS} from "../../../config/siteConfigIdentifiers";
 import {AUTHOR_CONTACTS} from "../../../config/config";
 import {getConfigurationsStore} from "../../../redux/slices/configurationsSlice";
+import {Typography} from "@mui/material";
 
 const Footer: FC = () => {
   const currentDate = new Date();
@@ -14,22 +15,24 @@ const Footer: FC = () => {
   return (
     <div className='footer-main-container'>
       <div className='main-center-content-container'>
-        <span
+        <Typography
           dangerouslySetInnerHTML={{__html: configurations.configurations[SITE_CONFIG_IDENTIFIERS.FOOTER_FIRST]?.value || ''}}
         />
-        <span
+        <Typography
           dangerouslySetInnerHTML={{__html: configurations.configurations[SITE_CONFIG_IDENTIFIERS.FOOTER_SECOND]?.value || ''}}
         />
-        <span
+        <Typography
           dangerouslySetInnerHTML={{__html: configurations.configurations[SITE_CONFIG_IDENTIFIERS.FOOTER_THIRD]?.value || ''}}
         />
       </div>
       <div className='copyright-container'>
         {configurations.status === STORE_STATUSES.COMPLETE ?
           !!configurations.configurations[SITE_CONFIG_IDENTIFIERS.DEMO_MODE]?.value ?
-            <a href={AUTHOR_CONTACTS.VK} target='_blank' style={{color: 'white'}}>© BarenAD</a>
+            <Typography>
+              <a href={AUTHOR_CONTACTS.VK} target='_blank' style={{color: 'white'}}>© BarenAD</a>
+            </Typography>
             :
-            <span>Copyright © 2011-{currentDate.getFullYear()}. Сладкая Радость</span>
+            <Typography>Copyright © 2011-{currentDate.getFullYear()}. Сладкая Радость</Typography>
           :
           null
         }
